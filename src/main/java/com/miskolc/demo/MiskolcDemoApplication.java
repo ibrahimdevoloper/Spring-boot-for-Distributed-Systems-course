8092d41df7552a3359ae28cfec09401ca52f914e
Ibrahim Shaglil <ibrahimdevoloper@gmail.com>
2024-10-21 13:35:22 +0200

    Initial commit

---
+++ src/main/java/com/miskolc/demo/MiskolcDemoApplication.java
@@ -0,0 +1,13 @@
+package com.miskolc.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiskolcDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiskolcDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miskolc/demo/MiskolcDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miskolc.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiskolcDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
