bb3e63dfdfa25950f4dc2b59d155267a9c40fb53
Ibrahim Shaglil <ibrahimdevoloper@gmail.com>
2024-10-23 00:28:25 +0200

    Update PokemonCardTest.java

--- src/test/java/com/miskolc/demo/models/PokemonCardTest.java
+++ src/test/java/com/miskolc/demo/models/PokemonCardTest.java
@@ -1,6 +1,5 @@
-package com.miskolc.demo;
+package com.miskolc.demo.models;
 
-import com.miskolc.demo.models.PokemonCard;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
