7d03603f8ce0a6f1e4f0bd3d36f094baedf1878d
Ibrahim Shaglil <ibrahimdevoloper@gmail.com>
2024-10-23 16:52:15 +0200

    Update PokemonCard.java

--- src/main/java/com/miskolc/demo/models/PokemonCard.java
+++ src/main/java/com/miskolc/demo/models/PokemonCard.java
@@ -1,14 +1,25 @@
 package com.miskolc.demo.models;
 
 import lombok.Data;
+import org.springframework.context.annotation.Primary;
 
 @Data
 public class PokemonCard {
+//    static private int mainId=1;
+    private int id;
     private String name;
     private int price;
     private String type;
+//    public PokemonCard(String name, int price, String type) {
+//        mainId++;
+//        this.id=mainId;
+//        this.name = name;
+//        this.price = price;
+//        this.type = type;
+//    }
 
-    public PokemonCard(String name, int price, String type) {
+    public PokemonCard(int id,String name, int price, String type) {
+        this.id=id;
         this.name = name;
         this.price = price;
         this.type = type;
