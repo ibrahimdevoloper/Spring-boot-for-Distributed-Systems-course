1e21f967a9510ad47285c317de83787c5642f491
Ibrahim Shaglil <ibrahimdevoloper@gmail.com>
2024-12-15 18:32:40 +0100

    added eureka

--- src/main/java/com/miskolc/demo/MiskolcDemoApplication.java
+++ src/main/java/com/miskolc/demo/MiskolcDemoApplication.java
@@ -1,10 +1,17 @@
 package com.miskolc.demo;
 
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Lazy;
 
 @SpringBootApplication
 public class MiskolcDemoApplication {
+
+	@Autowired
+	@Lazy
+	private EurekaClient eurekaClient;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MiskolcDemoApplication.class, args);
