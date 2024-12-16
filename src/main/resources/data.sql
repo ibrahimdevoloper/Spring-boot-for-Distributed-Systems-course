55eee3d8d716e6822be72c6182d8d8c9ec902cf6
Ibrahim Shaglil <ibrahimdevoloper@gmail.com>
2024-12-16 03:31:13 +0100

    added SQL commands

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO pokemon_card (name, price, type) VALUES ('Kyurem', 100, 'Dragon');
+INSERT INTO pokemon_card (name, price, type) VALUES ('Charmender', 50, 'Fire');
+INSERT INTO pokemon_card (name, price, type) VALUES ('Chazard', 70, 'Fire');
+INSERT INTO pokemon_card (name, price, type) VALUES ('Pichu', 30, 'Electric');
+INSERT INTO pokemon_card (name, price, type) VALUES ('Pickachu', 40, 'Electric');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE pokemon_card
+(
+    id    BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name  VARCHAR(255),
+    price BIGINT,
+    type  VARCHAR(255)
+);
