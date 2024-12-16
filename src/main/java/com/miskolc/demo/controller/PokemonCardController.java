c34d64f5896d0d012336d5f33a982e7f3ebfd7bb
Ibrahim Shaglil <ibrahimdevoloper@gmail.com>
2024-12-16 03:33:01 +0100

    modified controller

--- src/main/java/com/miskolc/demo/controller/PokemonCardController.java
+++ src/main/java/com/miskolc/demo/controller/PokemonCardController.java
@@ -1,6 +1,8 @@
 package com.miskolc.demo.controller;
 
 import com.miskolc.demo.models.PokemonCard;
+import com.miskolc.demo.services.PokemonCardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,23 +14,28 @@ import static org.springframework.http.ResponseEntity.notFound;
 @RestController
 @RequestMapping("/cards")
 public class PokemonCardController {
-    List<PokemonCard> cards;
-    public PokemonCardController(){
-        cards = new LinkedList<>();
-        cards.add(new PokemonCard("Kyurem",100,"Holo"));
-        cards.add(new PokemonCard("Charmender",10,"Normal"));
-        cards.add(new PokemonCard("Pickachu",50,"Ghost"));
+    //    List<PokemonCard> cards;
+    @Autowired
+    PokemonCardService pokemonCardService;
+
+    public PokemonCardController() {
+//        cards = new LinkedList<>();
+//        cards.add(new PokemonCard("Kyurem",100,"Holo"));
+//        cards.add(new PokemonCard("Charmender",10,"Normal"));
+//        cards.add(new PokemonCard("Pickachu",50,"Ghost"));
     }
+
     @GetMapping("/{id}")
     private ResponseEntity<PokemonCard> findById(@PathVariable int id) {
-        PokemonCard card = null;
-        for(int j=0; j<cards.size();j++){
-            PokemonCard temp = cards.get(j);
-            if (temp.getId() == id) {
-                card =temp;
-                break;
-            }
-        }
+//        PokemonCard card = null;
+//        for(int j=0; j<cards.size();j++){
+//            PokemonCard temp = cards.get(j);
+//            if (temp.getId() == id) {
+//                card =temp;
+//                break;
+//            }
+//        }
+        PokemonCard card = pokemonCardService.getPokemonCard(id);
         if (card == null)
             return (ResponseEntity<PokemonCard>) notFound();
         return ResponseEntity.ok(card);
@@ -36,6 +43,7 @@ public class PokemonCardController {
 
     @GetMapping(path = "")
     private ResponseEntity<List<PokemonCard>> findAll() {
+        List<PokemonCard> cards = pokemonCardService.getAllPokemonCards();
         if (cards.size() < 1)
             return (ResponseEntity<List<PokemonCard>>) notFound();
         return ResponseEntity.ok(cards);
@@ -43,37 +51,39 @@ public class PokemonCardController {
 
     @PostMapping(path = "")
     private ResponseEntity<PokemonCard> addPokemon(@RequestBody PokemonCard card) {
-            System.out.println("add Pokemon");
-            cards.add(card);
-            return ResponseEntity.ok(card);
+        System.out.println("add Pokemon");
+        pokemonCardService.addPokemonCard(card);
+        return ResponseEntity.ok(card);
     }
 
     @PutMapping(path = "")
     private ResponseEntity<PokemonCard> editPokemon(@RequestBody PokemonCard card) {
-        for(int j=0; j<cards.size();j++){
-            PokemonCard temp = cards.get(j);
-            if (temp.getId() == card.getId()) {
-                cards.get(j).setName(card.getName());
-                cards.get(j).setPrice(card.getPrice());
-                cards.get(j).setType(card.getType());
-                break;
-            }
-        }
-        if (card == null)
-            return (ResponseEntity<PokemonCard>) notFound();
+//        for (int j = 0; j < cards.size(); j++) {
+//            PokemonCard temp = cards.get(j);
+//            if (temp.getId() == card.getId()) {
+//                cards.get(j).setName(card.getName());
+//                cards.get(j).setPrice(card.getPrice());
+//                cards.get(j).setType(card.getType());
+//                break;
+//            }
+//        }
+        pokemonCardService.updatePokemonCard(card);
+//        if (card == null)
+//            return (ResponseEntity<PokemonCard>) notFound();
         return ResponseEntity.ok(card);
     }
 
     @DeleteMapping("/{id}")
     private ResponseEntity<String> deleteById(@PathVariable int id) {
-        PokemonCard card = null;
-        for(int j=0; j<cards.size();j++){
-            PokemonCard temp = cards.get(j);
-            if (temp.getId() == id) {
-                cards.remove(j);
-                break;
-            }
-        }
+//        PokemonCard card = null;
+//        for (int j = 0; j < cards.size(); j++) {
+//            PokemonCard temp = cards.get(j);
+//            if (temp.getId() == id) {
+//                cards.remove(j);
+//                break;
+//            }
+//        }
+        pokemonCardService.deletePokemonCard(id);
         return ResponseEntity.ok("{\"message\":\"Deleted\"}");
     }
 }
