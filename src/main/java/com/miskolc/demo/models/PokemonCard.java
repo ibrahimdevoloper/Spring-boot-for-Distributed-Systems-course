6b827eeacb01d579ca708580d7eba957bba5a7e0
Ibrahim Shaglil <ibrahimdevoloper@gmail.com>
2024-12-16 03:31:50 +0100

    added entity

--- src/main/java/com/miskolc/demo/models/PokemonCard.java
+++ src/main/java/com/miskolc/demo/models/PokemonCard.java
@@ -1,29 +1,26 @@
 package com.miskolc.demo.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.context.annotation.Primary;
 
 @Data
+@Entity
+@Table(name = "PokemonCard")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class PokemonCard {
-    static private int mainId=0;
+//    static private int mainId=0;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     private String name;
     private int price;
     private String type;
 
-    //JAXON default constructor
-    public PokemonCard(String name, int price, String type) {
-        mainId++;
-        this.id=mainId;
-        this.name = name;
-        this.price = price;
-        this.type = type;
-    }
-
-//    public PokemonCard(int id,String name, int price, String type) {
-//        this.id=id;
-//        this.name = name;
-//        this.price = price;
-//        this.type = type;
-//    }
 }
